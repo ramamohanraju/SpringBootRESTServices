03d509783f183f5e379df7ce5c63ff7e26e83185
Ramamohan Raju Gadiraju <g.ramamohanraju@gmail.com>
2018-12-22 18:45:16 +0530

    Initial sources

---
+++ src/main/java/com/example/springboot/explore/rest/SpringBootRESTServices/SpringBootRestServicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboot.explore.rest.SpringBootRESTServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServicesApplication.class, args);
+	}
+
+}
+
